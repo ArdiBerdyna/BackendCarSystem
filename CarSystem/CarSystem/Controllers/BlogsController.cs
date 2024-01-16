25d405887a9d273ae0450bd0b5a80127c787ac1f
ArdiBerdyna <ardi_881@hotmail.com>
2024-01-16 03:38:04 +0100

    implementimi i BlogCotroller

---
+++ CarSystem/CarSystem/Controllers/BlogsController.cs
@@ -0,0 +1,121 @@
+﻿using CarSystem.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly CarsDbContext _context;
+
+        public BlogsController(CarsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cars
+        [HttpGet]
+        [Route("GetBlogs")]
+
+        public async Task<ActionResult<IEnumerable<Blog>>> GetBlogs()
+        {
+            if (_context.Blogs == null)
+            {
+                return NotFound();
+            }
+            var ardi = await _context.Blogs.ToListAsync();
+            return ardi;
+        }
+
+
+        // GET: api/Cars/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Blog>> GetBlogById(int id)
+        {
+            if (_context.Blogs == null)
+            {
+                return NotFound();
+            }
+            var blog = await _context.Blogs.FindAsync(id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return blog;
+        }
+
+        // PUT: api/Cars/5
+
+        [HttpPut]
+        public IActionResult UpdateBlog(Blog product)
+        {
+            try
+            {
+                if (product == null)
+                    return StatusCode(StatusCodes.Status404NotFound);
+                else
+                {
+                    _context.Blogs.Update(product);
+                    _context.SaveChanges();
+                }
+                return StatusCode(StatusCodes.Status200OK);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+
+
+
+        [HttpPost]
+        [Route("PostBlog")]
+        public async Task<ActionResult<Car>> PostBlog(Blog blog)
+        {
+            _context.Blogs.Add(blog);
+
+
+
+
+            await _context.SaveChangesAsync();
+
+
+            return Ok(blog);
+
+        }
+
+
+
+        // DELETE: api/Cars/5
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteBlog(int id)
+        {
+            if (_context.Blogs == null)
+            {
+                return NotFound();
+            }
+            var blog = await _context.Blogs.FindAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            _context.Blogs.Remove(blog);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BlogExists(int id)
+        {
+            return (_context.Blogs?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CarSystem/CarSystem/Migrations/20240116015425_changeBlog.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarSystem.Migrations
+{
+    public partial class changeBlog : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Blogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Description",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
